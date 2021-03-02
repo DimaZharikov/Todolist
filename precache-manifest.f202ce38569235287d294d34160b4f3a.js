ef3deae605a2d9fd39bbc85b997f8b418f606469
DimaZharikov <DimaZharikov@users.noreply.github.com>
2021-03-02 09:18:01 +0300

    Updates

---
+++ precache-manifest.f202ce38569235287d294d34160b4f3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d027f8b267080a6133283ee047bc4888",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "37946ff9c0ab41d43917",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "280578e087c09568ee28",
+    "url": "/todolist/static/js/2.f7bb7785.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist/static/js/2.f7bb7785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37946ff9c0ab41d43917",
+    "url": "/todolist/static/js/main.05bc51b8.chunk.js"
+  },
+  {
+    "revision": "c3d5ccae1ee3a7bf91ff",
+    "url": "/todolist/static/js/runtime-main.16223656.js"
+  }
+]);
